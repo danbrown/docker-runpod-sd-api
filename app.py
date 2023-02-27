879affc7b760754db8399c5fbdf26ba3f2b3c70c
Daniel Brown <danielbrown25.9.2@gmail.com>
2023-02-27 09:14:42 -0300

    trying model preload

--- app.py
+++ app.py
@@ -9,17 +9,20 @@ import time
 HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
 
 # models
-MODEL_IDS = [
-  "runwayml/stable-diffusion-v1-5",
-  "hakurei/waifu-diffusion",
-]
-
-FP16_MODELS = [
-  "runwayml/stable-diffusion-v1-5",
-  "hakurei/waifu-diffusion",
-  "runwayml/stable-diffusion-inpainting",
-  "stabilityai/stable-diffusion-2"
-]
+MODEL_DATA = {
+  "runwayml/stable-diffusion-v1-5": {
+    "model_id": "runwayml/stable-diffusion-v1-5",
+    "slug": "sd-v1-5",
+    "precision": "fp16",
+    "revision": "fp16",
+  },
+  "hakurei/waifu-diffusion": {
+    "model_id": "hakurei/waifu-diffusion",
+    "slug": "waifu-diffusion",
+    "precision": "fp16",
+    "revision": "fp16",
+  },
+}
 
 # schedulers
 from diffusers import schedulers as _schedulers
@@ -34,6 +37,10 @@ SCHEDULERS = [
 
 DEFAULT_SCHEDULER = os.getenv("DEFAULT_SCHEDULER", SCHEDULERS[0])
 
+# globals
+global loaded_models
+
+# utils
 def decodeBase64Image(imageStr: str, name: str) -> PIL.Image:
   image = PIL.Image.open(BytesIO(base64.decodebytes(bytes(imageStr, "utf-8"))))
   print(f'Decoded image "{name}": {image.format} {image.width}x{image.height}')
@@ -66,7 +73,7 @@ def getScheduler(model_id: str, scheduler_id: str, load: bool) -> str:
   return inittedScheduler
 
 # load model
-def loadModel(model_id: str):
+def loadModel(model_data: str):
 
   start = time.time()
 
@@ -74,38 +81,60 @@ def loadModel(model_id: str):
 
   # scheduler = getScheduler(model_id, DEFAULT_SCHEDULER, False)
 
-  model = loadPipeline.from_pretrained(
+  model_id = model_data["model_id"]
+
+  pipe = loadPipeline.from_pretrained(
     model_id, 
-    torch_dtype=None if model_id not in FP16_MODELS else torch.float16,
+    torch_dtype=torch.float16 if model_data["precision"] == "fp16" else None,
+    revision="fp16" if model_data["revision"] == "fp16" else None,
     use_auth_token=HF_AUTH_TOKEN,
     # scheduler=scheduler,
   )
 
+  # save model
+  pipe.save_pretrained(f"./{model_data['slug']}")
+
   load_time = round((time.time() - start) * 1000)
   print(f"Loaded {model_id} in {load_time}ms")
 
-  return model.to("cuda") # TODO make this configurable
+  return pipe.to("cuda") # TODO make this configurable
 
 
 # # This is called once when the server starts
 def init():
   
   # load models
-  models = {}
-  for model_id in MODEL_IDS:
-    models[model_id] = loadModel(model_id)
+  for model_data in MODEL_DATA:
+    model_id = model_data["model_id"]
+    loaded_models[model_id] = loadModel(model_data)
 
   pass
 
 # # This is the main inference function that is called by the server
 def inference(model_inputs):
-  model_id = model_inputs.get("model_id", MODEL_IDS[0])
+  model_id = model_inputs.get("model_id", None)
   prompt = model_inputs.get("prompt", None)
   negative_prompt = model_inputs.get("negative_prompt", None)
   scheduler_id = model_inputs.get("scheduler_id", DEFAULT_SCHEDULER)
   width = model_inputs.get("width", 512)
   height = model_inputs.get("height", 512)
+  num_inference_steps = model_inputs.get("num_inference_steps", 30)
+  guidance_scale = model_inputs.get("guidance_scale", 7.5)
 
+  # special properties to trigger pipelines
+  init_image = model_inputs.get("init_image", None)
+  mask_image = model_inputs.get("mask_image", None)
+  controlnet_hint = model_inputs.get("controlnet_hint", None)
+
+  # seed generator
+  seed = model_inputs.get("seed", None)
+  if seed == None:
+      generator = torch.Generator(device=device)
+      generator.seed()
+  else:
+      generator = torch.Generator(device=device).manual_seed(seed)
+
+  # if prompt is not provided, return error
   if prompt == None:
     return {
       "error": {
@@ -114,8 +143,25 @@ def inference(model_inputs):
       }
     }
 
+  # check if model is valid, if not return error
+  if model_id not in MODEL_DATA.keys():
+    return {
+      "error": {
+        "code": "INVALID_MODEL",
+        "message": "Invalid model"
+      }
+    }
+
+  # if model is not loaded, load it
+  if model_id not in loaded_models:
+    loaded_models[model_id] = loadModel(model_id)
+
+
+  # get model
+  model_data = MODEL_DATA[model_id]
+
   pipe = StableDiffusionPipeline.from_pretrained(
-    model_id,
+    model_data["slug"],
     use_auth_token=HF_AUTH_TOKEN,
   )
 
@@ -128,12 +174,19 @@ def inference(model_inputs):
     negative_prompt=negative_prompt,
     width=width,
     height=height,
+    num_inference_steps=num_inference_steps,
+    guidance_scale=guidance_scale,
+    generator=generator,
   ).images
       
   # convert to base64
   images_base64 = []
   for image in images:
     images_base64.append("data:image/png;base64," + encodeBase64Image(image))
+
+  # TEMP save images locally for debugging
+  for i, image in enumerate(images):
+    image.save(f"./{i}.png")
 
   return {
     "images": images_base64
