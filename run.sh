75d33f72a16c11865b860f5f4165f380f8c15a5e
Daniel Brown <danielbrown25.9.2@gmail.com>
2023-02-26 23:03:36 -0300

    v2

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ git clone https://github.com/danbrown/docker-runpod-sd-api.git api
 # install the requirements, it is already installed in the docker image, but just in case
 pip install -r api/requirements.txt
 
-# runs the server in the background, if the SERVER_AUTOSTART is set to true
-if [ "$SERVER_AUTOSTART" = true ] ; then
+# runs the server in the background, if the SERVER_AUTOSTART is set
+if [ $SERVER_AUTOSTART ] ; then
     python api/server.py &
 fi
